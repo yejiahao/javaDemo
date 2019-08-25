75ba2bea2420393d02ac25fa067af652e2b10fed
yejiahao <454696086@qq.com>
2019-08-25 23:07:22 +0800

    :white_check_mark: update BinaryTreeCase

--- src/main/java/org/algorithm/tree/BinaryTreeCase.java
+++ src/main/java/org/algorithm/tree/BinaryTreeCase.java
@@ -1,7 +1,6 @@
 package org.algorithm.tree;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -54,11 +53,8 @@ public class BinaryTreeCase {
      * 给定一个非空二叉树，返回一个由每层节点平均值组成的数组。
      */
     public List<Double> averageOfLevels(TreeNode root) {
-        int level = 0;
         List<List<Integer>> list = new ArrayList<>();
-        list.add(level++, Collections.singletonList(root.val));
-        averageOfLevels(root.left, list, level);
-        averageOfLevels(root.right, list, level);
+        averageOfLevels(root, list, 0);
         return list.stream()
                 .map(v -> v.stream()
                         .mapToDouble(Integer::doubleValue)
@@ -107,6 +103,40 @@ public class BinaryTreeCase {
             rightDepth = maxDepth(root.right, i + 1);
         }
         return Math.max(leftDepth, rightDepth);
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/univalued-binary-tree/
+     * <p>
+     * 如果二叉树每个节点都具有相同的值，那么该二叉树就是单值二叉树。
+     */
+    public boolean isUnivalTree(TreeNode root) {
+        return Objects.isNull(root) // 只用于根节点
+                || (Objects.isNull(root.left) || root.val == root.left.val && isUnivalTree(root.left)) // 左节点 result
+                && (Objects.isNull(root.right) || root.val == root.right.val && isUnivalTree(root.right)); // 右节点 result
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/binary-tree-tilt/
+     * <p>
+     * 给定一个二叉树，计算整个树的坡度。
+     * 一个树的节点的坡度定义即为，该节点左子树的结点之和和右子树结点之和的差的绝对值。空结点的的坡度是0。
+     * 整个树的坡度就是其所有节点的坡度之和。
+     */
+    public int findTilt(TreeNode root) {
+        if (Objects.isNull(root)) {
+            return 0;
+        }
+        List<Integer> list = new ArrayList<>();
+        findTilt(root, list);
+        return list.stream().mapToInt(i -> i).sum();
+    }
+
+    private int findTilt(TreeNode root, List<Integer> list) {
+        int lVal = Objects.isNull(root.left) ? 0 : root.left.val + findTilt(root.left, list);
+        int rVal = Objects.isNull(root.right) ? 0 : root.right.val + findTilt(root.right, list);
+        list.add(Math.abs(lVal - rVal));
+        return lVal + rVal;
     }
 }
 
