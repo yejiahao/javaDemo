d6718b2668a70a1b4e39a9ac116699d2c60ce972
yejiahao <454696086@qq.com>
2020-04-29 11:19:46 +0800

    :white_check_mark: update BinaryTreeCase

--- src/main/java/org/algorithm/tree/BinaryTreeCase.java
+++ src/main/java/org/algorithm/tree/BinaryTreeCase.java
@@ -188,6 +188,37 @@ public class BinaryTreeCase {
         }
         return result;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/leaf-similar-trees/
+     * <p>
+     * 请考虑一颗二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列
+     * <p>
+     * 如果有两颗二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。
+     * <p>
+     * 如果给定的两个头结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。
+     */
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        calLeafSimilar(root1, list1);
+        calLeafSimilar(root2, list2);
+        return list1.equals(list2);
+    }
+
+    private void calLeafSimilar(TreeNode root, List<Integer> list) {
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        if (Objects.nonNull(left)) {
+            calLeafSimilar(left, list);
+        }
+        if (Objects.nonNull(right)) {
+            calLeafSimilar(right, list);
+        }
+        if (Objects.isNull(left) && Objects.isNull(right)) {
+            list.add(root.val);
+        }
+    }
 }
 
 /**
