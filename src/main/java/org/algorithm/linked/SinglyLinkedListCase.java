f20e28b9edd80973cb3db4c0a8217af442d528d6
yejiahao <454696086@qq.com>
2020-05-08 17:51:06 +0800

    :white_check_mark: update SinglyLinkedListCase

--- src/main/java/org/algorithm/linked/SinglyLinkedListCase.java
+++ src/main/java/org/algorithm/linked/SinglyLinkedListCase.java
@@ -181,6 +181,26 @@ public class SinglyLinkedListCase {
         }
         return result;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+     * <p>
+     * 1290. 二进制链表转整数
+     * <p>
+     * 给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+     * <p>
+     * 请你返回该链表所表示数字的 <em>十进制值</em> 。
+     * 输入: head = [1,0,1]
+     * 输出: 5
+     */
+    public int getDecimalValue(ListNode head) {
+        int ret = head.val;
+        while (Objects.nonNull(head.next)) {
+            head = head.next;
+            ret = (ret << 1) + head.val;
+        }
+        return ret;
+    }
 }
 
 /**
